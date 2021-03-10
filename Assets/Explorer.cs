7728bbd8847f56e48a590ae500eb750c8e8351af
Jahonan <jedrzej.mrozowski@gmail.com>
2021-03-10 14:17:53 +0100

    Added controls, now You're able to move around and explore the fractal; Added simple color change over time

---
+++ Assets/Explorer.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explorer : MonoBehaviour
+{
+
+    public Material mat;
+    public Vector2 pos;
+    public float scale, angle;
+
+    private Vector2 smoothPos;
+    private float smoothScale;
+    private float smoothAngle;
+
+    private void UpdateShader()
+    {
+        smoothPos = Vector2.Lerp(smoothPos, pos, .03f);
+        smoothScale = Mathf.Lerp(smoothScale, scale, .03f);
+        smoothAngle = Mathf.Lerp(smoothAngle, angle, .03f);
+
+        float aspect = (float)Screen.width / (float)Screen.height;
+
+        float scaleX = smoothScale;
+        float scaleY = smoothScale;
+
+        if(aspect>1f) scaleY /= aspect;
+        else scaleX *= aspect;
+
+        mat.SetVector("_Area", new Vector4(smoothPos.x, smoothPos.y, scaleX, scaleY));
+        mat.SetFloat("_Angle", smoothAngle);
+    }
+
+    private void HandleInputs()
+    {
+
+        //Zooming in on Keypad
+        if(Input.GetKey(KeyCode.KeypadPlus)) scale *= .99f;
+        if(Input.GetKey(KeyCode.KeypadMinus)) scale *= 1.01f;
+
+        if(Input.GetKey(KeyCode.Q)) angle += .01f;
+        if(Input.GetKey(KeyCode.E)) angle -= .01f;
+        
+        Vector2 dir = new Vector2(.01f*scale, 0);
+        float s = Mathf.Sin(angle);
+        float c = Mathf.Cos(angle);
+        dir = new Vector2(dir.x*c, dir.x*s);
+        
+
+        //Moving left/right with A and D
+        if(Input.GetKey(KeyCode.A)) pos -= dir;
+        if(Input.GetKey(KeyCode.D)) pos += dir;
+
+        dir = new Vector2(-dir.y, dir.x);
+
+        //Moving up/down with W and S
+        if(Input.GetKey(KeyCode.S)) pos -= dir;
+        if(Input.GetKey(KeyCode.W)) pos += dir;
+    }
+
+    void Start()
+    {
+        smoothScale=scale;
+    }
+
+    // FixedUpdate is called once per tick (not on every frame)
+    void FixedUpdate()
+    {
+        HandleInputs();
+        UpdateShader();  
+    }
+}
--- Assets/Mandelbrot.shader
+++ Assets/Mandelbrot.shader
@@ -4,6 +4,10 @@
     {
         _MainTex ("Texture", 2D) = "white" {}
         _Area( "Area", vector) = (0, 0, 4, 4)
+        _Angle ("Angle", range(-3.1415, 3.1415)) = 0
+        _MaxIter ("Iterations", range(4, 1000)) = 255
+        _Color ("Color", vector) = (.3, .45, .65, 1)
+        _Speed ("Speed", float) = .01
     }
     SubShader
     {
@@ -38,22 +42,48 @@
                 return o;
             }
 
-            float4 _Area;
+            float4 _Area, _Color;
+            float _Angle, _MaxIter, _Speed;
             sampler2D _MainTex;
+
+            float2 rot(float2 p, float2 pivot, float a)
+            {
+                float s = sin(a);
+                float c = cos(a);
+
+                p -= pivot;
+                p = float2(p.x*c-p.y*s, p.x*s+p.y*c);
+                p += pivot;
+
+                return p;
+            }
 
             fixed4 frag (v2f i) : SV_Target
             {
                 float2 c = _Area.xy + (i.uv-.5)*_Area.zw;
+                c = rot(c, _Area.xy, _Angle);
+
+                float r = 20;
+                float r2 = r*r;
+                
                 float2 z;
                 float iter;
 
-                for (iter = 0; iter < 255; iter++)
+                for (iter = 0; iter < _MaxIter; iter++)
                 {
                     z = float2(z.x*z.x-z.y*z.y, 2*z.x*z.y) + c;
-                    if (length(z) > 2) break;
+                    if (length(z) > r) break;
                 }
+                if (iter >= _MaxIter) return 0;
+                
+                float dist = length(z);
+                float fracIter = log2( log(dist)/log(r) ) -1;
 
-                return iter / 255;
+                iter -= fracIter;
+
+                float m = sqrt(iter / _MaxIter);
+                float4 col = sin(_Color*m*40*(_SinTime.y+2))*.5+.5;
+                return col;
             }
             ENDCG
         }
